4e252b592a081acb085b219118508310828ceda1
zhixuan.chen <zhixuan.chen@vmgpu011.hogpu.cc>
2018-02-03 15:01:55 +0800

    Fix mobilenet v2
    1. Fix residual structure
    2. In order to keep feature maps with default boxes, add 2 convolutional
    layers and add number filters in previous residual unit

--- symbol/mobilenetv2.py
+++ symbol/mobilenetv2.py
@@ -11,13 +11,12 @@ def Conv(data, num_filter=1, kernel=(1, 1), stride=(1, 1), pad=(0, 0), num_group
     return act 
 
 def inv_unit(data, num_filter, stride, num_group, name=None, suffix=''):
-    conv_pw1 = mx.sym.Convolution(data=data, num_filter=num_filter*4, kernel=(1, 1),stride=(1,1), pad=(0,0), name='%s%s_pw1' %(name, suffix))
+    conv_pw1 = mx.sym.Convolution(data=data, num_filter=num_filter*6, kernel=(1, 1),stride=(1,1), pad=(0,0), name='%s%s_pw1' %(name, suffix))
     act1 = mx.sym.Activation(data=conv_pw1, act_type='relu',name='%s%s_relu1' %(name,suffix))
-    conv_dw = mx.sym.Convolution(data=act1, num_filter=num_filter*4, num_group=num_group*4,kernel=(3, 3), stride=(stride,stride), pad=(1, 1), name='%s%s_dw' %(name, suffix))
+    conv_dw = mx.sym.Convolution(data=act1, num_filter=num_filter*6, num_group=num_group*6,kernel=(3, 3), stride=(stride,stride), pad=(1, 1), name='%s%s_dw' %(name, suffix))
     act2 = mx.sym.Activation(data=conv_dw, act_type='relu', name='%s%s_relu2' %(name,suffix))
     conv_pw2 = mx.sym.Convolution(data=act2, num_filter=num_filter, kernel=(1, 1),stride=(1,1), pad=(0, 0), name='%s%s_pw2' %(name,suffix))
     shortcut = mx.sym.Convolution(data=data,num_filter=num_filter,kernel=(1, 1),stride=(stride,stride),no_bias=True,name='%s%s_sc' %(name,suffix))
-    #inv = mx.symbol.concat(conv_pw2,shortcut,dim=1,name='%s%s_res' %(name,suffix))
     return conv_pw2+shortcut
 
 def get_symbol(num_classes, **kwargs):
@@ -25,12 +24,14 @@ def get_symbol(num_classes, **kwargs):
     conv_1 = Conv(data, num_filter=32, kernel=(3, 3),pad=(1, 1), stride=(2, 2),name="conv_1")
     conv_2 = inv_unit(conv_1, 32, 1, 32, name='conv_2')
     conv_3 = inv_unit(conv_1, 32, 2, 32, name='conv_3')
-    conv_4 = inv_unit(conv_3, 48, 1, 48, name='conv_4')
-    conv_5 = inv_unit(conv_4, 48, 2, 48, name='conv_5')
-    conv_6 = inv_unit(conv_5, 64, 1, 64, name='conv_6')
-    conv_7 = inv_unit(conv_6, 64, 2, 64, name='conv_7')
-    conv_8 = inv_unit(conv_7, 128, 1, 128, name='conv_8')
-    conv_9 = inv_unit(conv_8, 128, 2, 128, name='conv_9')
+    conv_4 = inv_unit(conv_3, 64, 1, 64, name='conv_4')
+    conv_5 = inv_unit(conv_4, 64, 2, 64, name='conv_5')
+    conv_6 = inv_unit(conv_5, 128, 1, 128, name='conv_6')
+    conv_7 = inv_unit(conv_6, 128, 2, 128, name='conv_7')
+    #conv_8 = inv_unit(conv_7, 128, 1, 128, name='conv_8')
+    conv_8 = Conv(conv_7, num_filter=256, kernel=(1, 1),pad=(0, 0),stride=(1, 1),name="conv_8")
+    #conv_9 = inv_unit(conv_8, 128, 2, 128, name='conv_9')
+    conv_9 = Conv(conv_8, num_filter=512, kernel=(3, 3),pad=(1, 1),stride=(2, 2),name="conv_9")
     ##normal convolution
     conv_10 = Conv(conv_9, num_filter=512, kernel=(1, 1),pad=(0, 0),stride=(1, 1),name="conv_10")
     pool = mx.sym.Pooling(data=conv_10, kernel=(1, 1),stride=(1, 1), pool_type="avg", name="gap",global_pool=True)
